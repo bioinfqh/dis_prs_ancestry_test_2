61493bdf51bf186f3d17503f2ef669684b8878f9
bioinfqh <79404382+bioinfqh@users.noreply.github.com>
2021-08-22 14:41:41 +0200

    Update run_prs.sh

--- run_prs.sh
+++ run_prs.sh
@@ -6,6 +6,6 @@ disease_list_path=$3
 
 #patient_id=$4
 
-patient_id=$(python3 get_id_from_vcf.py $patient_vcf)
-bash prs_and_report.sh $bg_dataset $disease_list_path $patient_id f
+patient_id=$(python3 /scripts/get_id_from_vcf.py $patient_vcf)
+bash /scripts/prs_and_report.sh $bg_dataset $disease_list_path $patient_id f
 
