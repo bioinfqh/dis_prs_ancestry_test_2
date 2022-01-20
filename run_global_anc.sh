a5158ecef1304e33609ce2e98e3620a0dac6ea4d
bioinfqh <79404382+bioinfqh@users.noreply.github.com>
2022-01-20 14:22:33 +0100

    Delete run_global_anc.sh

--- run_global_anc.sh
+++
@@ -1,7 +0,0 @@
-customer_id=$1
-input_file=$2
-
-bash /scripts/get_ancestry.sh $input_file /scripts/ancestry_map_$customer_id.html /scripts/output_$customer_id.Q
-python3 /scripts/read_ancestry_file.py /scripts/output_$customer_id.Q /scripts/ancestry_$customer_id.json
-
-
